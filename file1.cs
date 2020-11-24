76b80ecb679af62c3e5dbb330cfa08fe4e465bb7
Arsalan <sonicspeed848@gmail.com>
2020-11-24 00:33:13 +0000

    Create file1.cs

---
+++ file1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics;
+
+namespace file1
+{
+    class Program
+    {
+        int Main()
+        {
+            Process.Start("./file2.c");
+        }
+    }
+}
